27c2431962ab518575717126020e1920168e22f3
clemens <dcleme51@gmail.com>
2023-12-18 21:50:30 +0000

    creating array of questions,write to file function and async function

--- starter/index.js
+++ starter/index.js
@@ -5,16 +5,39 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of you project?",
+
+    },
+    {
+    type: "input",
+    name: "description",
+    message: "Describe your project in summary",
+    },
+    
+    {
+        type: "list",
+        name: "licence",
+        message: "Please select your chosen licence",
+        choices: ["MIT", "Apache", "No Licence"],
+    }
 
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, function(error, data){
+        error ? console.log(error): console.log("readMe Generatered")
+    })
 }
 
 // function to initialize program
-function init() {
-
+async function init() {
+    const anwsers = await inquirer.prompt(questions);
+    // console.log(anwsers)
+    writeToFile("README.md", generateMarkdown(anwsers));
 }
 
 // function call to initialize program
